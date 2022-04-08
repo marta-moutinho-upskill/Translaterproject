189689c88e08a4531b604913f9472c74b7475c48
Marta Moutinho <marta.barbosa-t22@upskill.pt>
2022-04-08 11:33:39 +0100

    app singleton

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,3 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class App {
+ private static App instance = null;
+
+ private App()
+ {
+//private to make sure no instanciation is done
+ }
+
+ public static App getInstance()
+ {
+  if (instance == null)
+  {
+   instance = new App();
+  }
+  return instance;
+ }
+
 
 }
